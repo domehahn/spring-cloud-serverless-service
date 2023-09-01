22ebc0e071ddbd1d14ff806c45d43f1e9deac150
Dominik Hahn <develop.illuminati@gmail.com>
2023-09-01 22:25:25 +0200

    update config

--- src/main/java/eu/aboutdev/microservice/serverlessservice/ServerlessServiceApplication.java
+++ src/main/java/eu/aboutdev/microservice/serverlessservice/ServerlessServiceApplication.java
@@ -2,8 +2,10 @@ package eu.aboutdev.microservice.serverlessservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServerlessServiceApplication {
 
     public static void main(String[] args) {
